1cf1d8f2063b8452b4d2caa8a0f8a5f2d5ca1dc7
Rodrigo Abreu <rodrigodabreu@edu.unisinos.br>
2025-09-22 21:41:54 -0300

    initial commit with project files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # i-am-rich
 Repo created for my first project with swift and ios development
+
+Project based in a Udemy course teached by Dr. Angela Yu
+
+First steps in IOS development
---
+++ iamrichTests/iamrichTests.swift
@@ -0,0 +1,17 @@
+//
+//  iamrichTests.swift
+//  iamrichTests
+//
+//  Created by Rodrigo Abreu on 9/22/25.
+//
+
+import Testing
+@testable import iamrich
+
+struct iamrichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
